416d0cbb26d6afcfda072fe256d2deee78b757ef
CampusTrace Dev <campustrace@dev.com>
2026-06-01 00:45:39 +0530

    feat: add client/src/components/GlassCard.jsx

---
+++ client/src/components/GlassCard.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const GlassCard = ({ children, className = '', hoverEffect = false, ...props }) => {
+  return (
+    <div
+      className={`glass ${hoverEffect ? 'glass-hover' : ''} ${className}`}
+      style={{
+        padding: '24px',
+        borderRadius: 'var(--radius-lg)',
+        position: 'relative',
+        overflow: 'hidden',
+      }}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default GlassCard;
